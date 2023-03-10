7482e20a91d5622077fbea86193d5bd53a896303
onward-web <developer@onward-web.com>
2023-03-10 20:51:45 +0200

    Update GeneratedEvent.php

--- src/Mariuzzo/LaravelJsLocalization/Events/GeneratedEvent.php
+++ src/Mariuzzo/LaravelJsLocalization/Events/GeneratedEvent.php
@@ -2,6 +2,10 @@
 
 namespace Mariuzzo\LaravelJsLocalization\Events;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class GeneratedEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
